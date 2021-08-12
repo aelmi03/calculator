f2800f9166b3408f5fe7cbffc1f695f9211895e1
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-08-12 04:23:08 -0700

    Added the ability to add numbers to the display and i made sure there is a limit of numbers and operators

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const numberButtons = document.querySelectorAll(".number, .operator");
+numberButtons.forEach(button => button.addEventListener("click", displayNumber));
+const mainText = document.querySelector("#mainText");
+const delButton = document.querySelector(`button[value = "DEL"]`);
+delButton.addEventListener("click", deleteNumber);
+let currentDisplayText = "";
+console.log(mainText);
+function deleteNumber(){
+    console.log(mainText.textContent.length)
+    if(mainText.textContent.length === 1){
+        console.log("lel");
+        mainText.textContent = "0";
+    }
+    else{
+    mainText.textContent = mainText.textContent.slice(0,mainText.textContent.length-1);
+    }
+}
+function displayNumber(e){
+    if(checkIfOperator(e) === true){
+        if(getAmountOfOperators(currentDisplayText) >= 1){
+            return;
+        }
+    }
+    if(mainText.textContent[0] === "0"){
+        mainText.textContent = e.target.value;
+        return;
+    }
+    if(mainText.textContent.length <= 14){
+      mainText.textContent += e.target.value;  
+      currentDisplayText = mainText.textContent;
+      console.log(currentDisplayText);
+    }
+}
+function checkIfOperator(e){
+    if(e.target.value === "+" ||e.target.value === "-" || e.target.value === "/" ||
+       e.target.value === "*" || e.target.value === "%"){
+           return true;
+       }
+       return false;
+}
+function getAmountOfOperators(string){
+    let count = 0;
+    for(let i = 0; i < string.length; i++){
+        if(string[i] === "+" || string[i] === "-" || string[i] === "/" ||
+           string[i] === "*" || string[i] === "%"){
+            count++;
+         }
+    }
+    return count;
+}
 function add(num1, num2){
     return num1 + num2;
 }
